bd2da08da701196e32a3318a8273e26a78498e89
villank2 <kylevillanueva1212@gmail.com>
2019-10-08 16:50:39 +0100

    update correct version of shortest.hs

--- shortest.hs
+++ shortest.hs
@@ -1,6 +1,8 @@
 shortest :: [[a]] -> [a]
-shortest [] = error "empty list"
-shortest (h:t) = short_helper h t where
-   short_helper x [] = x
-   short_helper x (xs:xy) | xs < x = short_helper xs xy
-                          | otherwise = short_helper x xy
+shortest [] = []
+shortest [x] = x
+shortest (x:xs)
+   | length x > length other_head = shortest xs
+   | otherwise = shortest (x : other_tail)
+   where other_head = head xs
+         other_tail = tail xs
