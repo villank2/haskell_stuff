da48032e4ee4ee603912696a09578e7e1e5f0e24
villank2 <kylevillanueva1212@gmail.com>
2019-10-18 13:29:44 +0100

    needs to be corrected

--- nubSorted.hs
+++ nubSorted.hs
@@ -1,4 +1,5 @@
 nubSorted :: Eq a => [a] -> [a]
 nubSorted n [] = []
+nubSorted n [h] = [h]
 nubSorted n (h:t) | h /= head tail = [h] ++ nubSorted n t
                   | otherwise = [] ++ nubSorted n t
